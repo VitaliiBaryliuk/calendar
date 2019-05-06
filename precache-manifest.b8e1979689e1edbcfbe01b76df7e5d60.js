8281b7d2f7392f2af0cf3ac55d43494e479f2c3b
VitaliiBaryliuk <vitaliyua912@gmail.com>
2019-05-06 04:49:48 +0300

    upoaded files for gh-pages

---
+++ precache-manifest.b8e1979689e1edbcfbe01b76df7e5d60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d19dac0ad449ca7b8c210ac6580f48a",
+    "url": "/forecast-app/static/media/background.7d19dac0.jpg"
+  },
+  {
+    "revision": "1b8ff24b22f74ad27fb6",
+    "url": "/forecast-app/static/js/runtime~main.cfa6f48a.js"
+  },
+  {
+    "revision": "c7ed8a444487092342c2",
+    "url": "/forecast-app/static/js/main.6de1647b.chunk.js"
+  },
+  {
+    "revision": "e941f3c39fa9845f13c9",
+    "url": "/forecast-app/static/js/2.ad3a9092.chunk.js"
+  },
+  {
+    "revision": "c7ed8a444487092342c2",
+    "url": "/forecast-app/static/css/main.34e04d9b.chunk.css"
+  },
+  {
+    "revision": "6128daad48c2a7d398717bcf0b343b55",
+    "url": "/forecast-app/index.html"
+  }
+];
---
+++ precache-manifest.f73e875ebf81f2d49fc54ab991667359.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6977a5f04cd60f18fb47c1ed8b2888fa",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "a6714cfe784437ace49e",
+    "url": "/calendar/static/css/main.7b7cc4d4.chunk.css"
+  },
+  {
+    "revision": "93852d93a75207da1062",
+    "url": "/calendar/static/js/2.613438f2.chunk.js"
+  },
+  {
+    "revision": "a6714cfe784437ace49e",
+    "url": "/calendar/static/js/main.1905625d.chunk.js"
+  },
+  {
+    "revision": "70cd18d5f6522f88cc6f",
+    "url": "/calendar/static/js/runtime~main.3b46a14a.js"
+  },
+  {
+    "revision": "51d8afaba8ac81d2b935a48e022cb94a",
+    "url": "/calendar/static/media/close-icon.51d8afab.png"
+  },
+  {
+    "revision": "0f7ddc24583501b1de6e5e652c6fb450",
+    "url": "/calendar/static/media/search-icon.0f7ddc24.png"
+  }
+]);
